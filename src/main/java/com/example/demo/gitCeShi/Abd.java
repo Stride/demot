3650949d36bf8858f01b50b1ea452fd853cd298d
yanglh <1101399787@qq.com>
2021-07-28 16:31:46 +0800

    修改类名

---
+++ src/main/java/com/example/demo/gitCeShi/Abd.java
@@ -0,0 +1,20 @@
+package com.example.demo.gitCeShi;
+
+/**
+ * @author admin
+ */
+public class Abd {
+
+
+
+    String a = "在GitHub中添加代码，测试pull";
+
+    String abc = "测试代码冲突";
+
+    public void aiYa(){
+
+        System.out.println(a);
+        System.out.println(abc);
+    }
+
+}
