77af693934a9274743aa14bb635ba016f3c0e9fb
yanglh <1101399787@qq.com>
2021-07-28 13:54:14 +0800

    第一次提叫

---
+++ src/main/java/com/example/demo/test/GuiFanTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.test;
+
+/**
+ * @author admin
+ */
+public class GuiFanTest {
+
+
+
+    public static void main(String[] args) {
+
+        String ab = "helle";
+
+        System.out.println(ab );
+
+
+    }
+
+}
